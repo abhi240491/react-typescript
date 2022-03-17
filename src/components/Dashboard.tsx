522f873c91b4611b4bc83b26e4b707eca89142c1
ABHISHEK KUMAR <sabhi8101@gmail.com>
2022-03-17 11:39:18 +0530

    added newsApi

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,5 +1,39 @@
-
+import {useState, useEffect} from 'react';
+import axios from 'axios';
 function NewsDashboard() {
+    type NewsFormat = {
+      source: {
+          id: String,
+          name: String
+      },
+      author: String,
+      title: String,
+      description: String,
+      url: String,
+      urlToImage: String,
+      publishedAt: String,
+      content: String,
+    };
+    
+    const [newsArray, setNewsArray] = useState<NewsFormat[]>([])
+    const [newsResults, setNewsResults] =useState<Number>(0)
+
+    const newsApi = async () => {
+        try {
+          const news = await axios.get(`https://newsapi.org/v2/top-headlines?country=in&apiKey=98d12b78cb574b75863b8267402465cf`);
+          console.log(news);
+          setNewsArray(news.data.articles);
+          setNewsResults(news.data.totalResults);
+        } catch (error) {
+          console.log(error);
+        }
+      };
+    
+      /* useEffect(() => {
+        newsApi();
+        // eslint-disable-next-line
+      }, [newsResults,newsArray]); */
+    
   return (
     <div className="dashboard">
         Dashboard
